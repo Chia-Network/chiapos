1f42713a31d3c7bba31fc57cfe97e852f5d70457
Gene Hoffman <hoffmang@hoffmang.com>
2020-03-30 02:44:25 -0700

    Iniital windows build.yml

---
+++ windows-build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -x
+
+# Stop in case of error
+set -e
+
+#python -m pip install cibuildwheel==1.3.0
+
+
+# build just python 3.7
+export CIBW_BUILD="cp37-win_amd64"
+#export CIBW_BEFORE_BUILD_WINDOWS="python -m pip install --upgrade pip"
+export CIBW_TEST_REQUIRES="pytest"
+export CIBW_TEST_COMMAND="py.test -v {project}/tests"
+
+
+export PATH=$PATH:"C:\msys64\mingw64\bin\cmake.exe"
+echo "PWD is $PWD"
+
+cmake -G "MSYS Makefiles" --build .
+make
+RunTests.exe
+#pip install pytest
+#Test failing for windows:
+py.test -v $PWD/tests/
+pip -vv wheel .
