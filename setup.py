a82b2b77a60e79f754c9ba9d6f5b5e7c9b7cea6b
Bill Blanke <wjb2002@flex.com>
2020-05-09 14:54:59 -0700

    removed global nominmax

--- setup.py
+++ setup.py
@@ -135,7 +135,7 @@ def cpp_flag(compiler):
 class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
     c_opts = {
-        'msvc': ['/EHsc','/std:c++17','/D','NOMINMAX','/O2'],
+        'msvc': ['/EHsc','/std:c++17','/O2'],
         'unix': [],
     }
     l_opts = {
