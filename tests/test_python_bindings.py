b31f60e1e3ea023a49b918c4e1134c26b13f062d
Gene Hoffman <hoffmang@hoffmang.com>
2020-09-02 11:24:47 -0700

    It should be setup_requires - fade to black...

--- tests/test_python_bindings.py
+++ tests/test_python_bindings.py
@@ -8,12 +8,47 @@ class TestPythonBindings(unittest.TestCase):
     def test_k_21(self):
         challenge: bytes = bytes([i for i in range(0, 32)])
 
-        plot_seed: bytes = bytes([5, 104, 52, 4, 51, 55, 23, 84, 91, 10, 111, 12, 13,
-                                  222, 151, 16, 228, 211, 254, 45, 92, 198, 204, 10, 9,
-                                  10, 11, 129, 139, 171, 15, 23])
+        plot_seed: bytes = bytes(
+            [
+                5,
+                104,
+                52,
+                4,
+                51,
+                55,
+                23,
+                84,
+                91,
+                10,
+                111,
+                12,
+                13,
+                222,
+                151,
+                16,
+                228,
+                211,
+                254,
+                45,
+                92,
+                198,
+                204,
+                10,
+                9,
+                10,
+                11,
+                129,
+                139,
+                171,
+                15,
+                23,
+            ]
+        )
 
         pl = DiskPlotter()
-        pl.create_plot_disk(".", ".", ".", "myplot.dat", 21, bytes([1, 2, 3, 4, 5]), plot_seed, 2*1024)
+        pl.create_plot_disk(
+            ".", ".", ".", "myplot.dat", 21, bytes([1, 2, 3, 4, 5]), plot_seed, 2 * 1024
+        )
         pl = None
 
         pr = DiskProver(str(Path("myplot.dat")))
@@ -28,13 +63,17 @@ class TestPythonBindings(unittest.TestCase):
             challenge = sha256(i.to_bytes(4, "big")).digest()
             for index, quality in enumerate(pr.get_qualities_for_challenge(challenge)):
                 proof = pr.get_full_proof(challenge, index)
-                assert len(proof) == 8*pr.get_size()
-                computed_quality = v.validate_proof(plot_seed, pr.get_size(), challenge, proof)
+                assert len(proof) == 8 * pr.get_size()
+                computed_quality = v.validate_proof(
+                    plot_seed, pr.get_size(), challenge, proof
+                )
                 assert computed_quality == quality
                 total_proofs += 1
 
-        print(f"total proofs {total_proofs} out of {iterations}\
-            {total_proofs / iterations}")
+        print(
+            f"total proofs {total_proofs} out of {iterations}\
+            {total_proofs / iterations}"
+        )
         assert total_proofs > 4000
         assert total_proofs < 6000
         pr = None
@@ -44,10 +83,13 @@ class TestPythonBindings(unittest.TestCase):
             for byte_block in iter(lambda: f.read(4096), b""):
                 sha256_plot_hash.update(byte_block)
             plot_hash = str(sha256_plot_hash.hexdigest())
-        assert plot_hash == "80e32f560f3a4347760d6baae8d16fbaf484948088bff05c51bdcc24b7bc40d9"
+        assert (
+            plot_hash
+            == "80e32f560f3a4347760d6baae8d16fbaf484948088bff05c51bdcc24b7bc40d9"
+        )
         print(f"\nPlotfile asserted sha256: {plot_hash}\n")
         Path("myplot.dat").unlink()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
