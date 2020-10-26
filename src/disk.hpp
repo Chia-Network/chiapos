c238ec581fddab84b93f3f783ef735be041b688c
Arvid Norberg <arvid@libtorrent.org>
2020-10-26 16:07:13 +0100

    Add BufferedFile, that incorporates read and write cache. This simplifies loops that iterate files, by not having them read larger chunks, but considering one entry at a time.

--- src/disk.hpp
+++ src/disk.hpp
@@ -215,4 +215,105 @@ private:
     FILE *f_;
 };
 
+struct BufferedDisk : Disk
+{
+    BufferedDisk(Disk* disk, uint64_t file_size) : disk_(disk), file_size_(file_size) {}
+
+    void Read(uint64_t begin, uint8_t *memcache, uint64_t length) override
+    {
+        if (read_buffer_start_ <= begin && read_buffer_start_ + read_buffer_size_ >= begin + length) {
+            // if the read is entirely inside the buffer, just memcopy it out
+            ::memcpy(memcache, read_buffer_ + (begin - read_buffer_start_), length);
+        }
+        else if (begin >= read_buffer_start_ + read_buffer_size_ || begin == 0) {
+            // if the read is beyond the current buffer (i.e.
+            // forward-sequential) move the buffer forward and read the next
+            // buffer-capacity number of bytes
+            read_buffer_start_ = begin;
+            uint64_t const amount_to_read = std::min(file_size_ - read_buffer_start_, read_buffer_capacity_);
+            disk_->Read(begin, read_buffer_, amount_to_read);
+            read_buffer_size_ = amount_to_read;
+            ::memcpy(memcache, read_buffer_ + (begin - read_buffer_start_), length);
+        }
+        else {
+            // if we're going backwards, don't wipe out the cache. We assume
+            // forward sequential access
+            disk_->Read(begin, memcache, length);
+        }
+    }
+
+    void Write(uint64_t const begin, const uint8_t *memcache, uint64_t const length) override
+    {
+        if (begin == write_buffer_start_ + write_buffer_size_) {
+            if (write_buffer_size_ + length <= write_buffer_capacity_) {
+                ::memcpy(write_buffer_ + write_buffer_size_, memcache, length);
+                write_buffer_size_ += length;
+                return;
+            }
+            TryFlushWriteCache();
+        }
+
+        if (write_buffer_size_ == 0 && write_buffer_capacity_ >= length) {
+            write_buffer_start_ = begin;
+            ::memcpy(write_buffer_ + write_buffer_size_, memcache, length);
+            write_buffer_size_ = length;
+            return;
+        }
+
+        disk_->Write(begin, memcache, length);
+    }
+
+    void Truncate(uint64_t const new_size) override
+    {
+        TryFlushWriteCache();
+        disk_->Truncate(new_size);
+        file_size_ = new_size;
+    }
+
+    std::string GetFileName() override { return disk_->GetFileName(); }
+
+    void SetReadCache(uint8_t* buffer, uint64_t size)
+    {
+        read_buffer_ = buffer;
+        read_buffer_capacity_ = size;
+        read_buffer_start_ = -1;
+        read_buffer_size_ = 0;
+    }
+
+    void SetWriteCache(uint8_t* buffer, uint64_t size)
+    {
+        write_buffer_ = buffer;
+        write_buffer_capacity_ = size;
+        write_buffer_start_ = -1;
+        write_buffer_size_ = 0;
+    }
+
+private:
+
+    void TryFlushWriteCache()
+    {
+        if (write_buffer_size_ == 0) return;
+
+        disk_->Write(write_buffer_start_, write_buffer_, write_buffer_size_);
+        write_buffer_size_ = 0;
+    }
+
+    Disk* disk_;
+
+    uint64_t file_size_;
+
+    // the file offset the read buffer was read from
+    uint64_t read_buffer_start_ = -1;
+    uint8_t* read_buffer_ = nullptr;
+    uint64_t read_buffer_size_ = 0;
+    uint64_t read_buffer_capacity_ = 0;
+
+    // the file offset the write buffer should be written back to
+    // the write buffer is *only* for contiguous and sequential writes
+    uint64_t write_buffer_start_ = -1;
+    uint8_t* write_buffer_ = nullptr;
+    uint64_t write_buffer_size_ = 0;
+    uint64_t write_buffer_capacity_ = 0;
+};
+
 #endif  // SRC_CPP_DISK_HPP_
