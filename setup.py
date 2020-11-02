0d9e49f3d4a565b05327bcf2b095e9ad05c90678
Bill Blanke <wjb2002@flex.com>
2020-11-01 18:09:06 -0800

    culling

--- setup.py
+++ setup.py
@@ -9,109 +9,16 @@ from setuptools import setup, setuptools, Extension
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
-
-class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=""):
-        Extension.__init__(self, name, sources=["./"])
-        self.sourcedir = os.path.abspath(sourcedir)
-
-
-class CMakeBuild(build_ext):
-    def run(self):
-        try:
-            out = subprocess.check_output(["cmake", "--version"])
-        except OSError:
-            raise RuntimeError(
-                "CMake must be installed to build"
-                + " the following extensions: "
-                + ", ".join(e.name for e in self.extensions)
-            )
-
-        if platform.system() == "Windows":
-            cmake_version = LooseVersion(
-                re.search(r"version\s*([\d.]+)", out.decode()).group(1)
-            )
-            if cmake_version < "3.1.0":
-                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
-
-        for ext in self.extensions:
-            self.build_extension(ext)
-
-    def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = [
-            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + str(extdir),
-            "-DPYTHON_EXECUTABLE=" + sys.executable,
-        ]
-
-        cfg = "Debug" if self.debug else "Release"
-        build_args = ["--config", cfg]
-
-        if platform.system() == "Windows":
-            cmake_args += [
-                "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
-            ]
-            if sys.maxsize > 2 ** 32:
-                cmake_args += ["-A", "x64"]
-            build_args += ["--", "/m"]
-        else:
-            cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
-            build_args += ["--", "-j", "6"]
-
-        env = os.environ.copy()
-        env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-            env.get("CXXFLAGS", ""), self.distribution.get_version()
-        )
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-        subprocess.check_call(
-            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env
-        )
-        subprocess.check_call(
-            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
-        )
-
-
-class get_pybind_include(object):
-    """Helper class to determine the pybind11 include path
-
-    The purpose of this class is to postpone importing pybind11
-    until it is actually installed, so that the ``get_include()``
-    method can be invoked."""
-
-    def __init__(self, user=False):
-        self.user = user
-
-    def __str__(self):
-        import pybind11
-
-        return pybind11.get_include(self.user)
-
-
 ext_modules = [
     Extension(
         "chiapos",
         [
-            "lib/FiniteStateEntropy/lib/entropy_common.c",
-            "lib/FiniteStateEntropy/lib/fse_compress.c",
-            "lib/FiniteStateEntropy/lib/fse_decompress.c",
-            "lib/FiniteStateEntropy/lib/hist.c",
             "python-bindings/chiapos.cpp",
-            "uint128_t/uint128_t.cpp",
-            "src/b3/blake3.c",
-            "src/b3/blake3_portable.c",
-            "src/b3/blake3_dispatch.c",
-            "src/b3/blake3_avx2.c",
-            "src/b3/blake3_avx512.c",
-            "src/b3/blake3_sse41.c",
-            "src/chacha8.c",
         ],
         include_dirs=[
             # Path to pybind11 headers
             get_pybind_include(),
             get_pybind_include(user=True),
-            "src",
-            "uint128_t",
             ".",
         ],
     ),
@@ -183,34 +90,18 @@ class BuildExt(build_ext):
         build_ext.build_extensions(self)
 
 
-if platform.system() == "Windows":
-    setup(
-        name="chiapos",
-        author="Mariano Sorgente",
-        author_email="mariano@chia.net",
-        description="Chia proof of space plotting, proving, and verifying (wraps C++)",
-        license="Apache License",
-        python_requires=">=3.7",
-        long_description=open("README.md").read(),
-        long_description_content_type="text/markdown",
-        url="https://github.com/Chia-Network/chiapos",
-        setup_requires=["pybind11"],
-        ext_modules=ext_modules,
-        cmdclass={"build_ext": BuildExt},
-        zip_safe=False,
-    )
-else:
-    setup(
-        name="chiapos",
-        author="Mariano Sorgente",
-        author_email="mariano@chia.net",
-        description="Chia proof of space plotting, proving, and verifying (wraps C++)",
-        license="Apache License",
-        python_requires=">=3.7",
-        long_description=open("README.md").read(),
-        long_description_content_type="text/markdown",
-        url="https://github.com/Chia-Network/chiavdf",
-        ext_modules=[CMakeExtension("chiapos", ".")],
-        cmdclass=dict(build_ext=CMakeBuild),
-        zip_safe=False,
-    )
+setup(
+    name="chiapos",
+    author="Mariano Sorgente",
+    author_email="mariano@chia.net",
+    description="Chia proof of space plotting, proving, and verifying (wraps C++)",
+    license="Apache License",
+    python_requires=">=3.7",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Chia-Network/chiapos",
+    setup_requires=["pybind11"],
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": BuildExt},
+    zip_safe=False,
+)
