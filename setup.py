867e8d305a43ca212a0826c04425f22703c07aac
Bill Blanke <wjb2002@flex.com>
2020-11-01 18:21:46 -0800

    fix

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
-import os
-import re
 import sys
-import platform
-import subprocess
 
 from setuptools import setup, setuptools, Extension
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
+
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked."""
+
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+
+        return pybind11.get_include(self.user)
+
 
 ext_modules = [
     Extension(
