dde55a3d5f2db57ad38c6eeae3bcf66215c7ba76
Arvid Norberg <arvid@libtorrent.org>
2020-12-09 12:44:15 +0100

    restore phase2 and phase3 to use uniform sort

--- src/phase3.hpp
+++ src/phase3.hpp
@@ -373,7 +373,7 @@ Phase3Results RunPhase3(
             filename + ".p3s.t" + std::to_string(table_index + 1),
             0,
             0,
-            strategy_t::quicksort);
+            strategy_t::quicksort_last);
 
         std::vector<uint8_t> park_deltas;
         std::vector<uint64_t> park_stubs;
