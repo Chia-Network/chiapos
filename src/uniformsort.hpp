ade1a91ab48169d01cbbd99ef0ace35a354ba82b
pechy <3456763+pechy@users.noreply.github.com>
2021-05-21 19:15:15 +0200

    Prefetch from disk when doing uniform sort

--- src/uniformsort.hpp
+++ src/uniformsort.hpp
@@ -23,10 +23,15 @@
 
 #include "./disk.hpp"
 #include "./util.hpp"
+#include "./threading.hpp"
+
+#include "thread_pool.hpp"
+
+extern thread_pool pool;
 
 namespace UniformSort {
 
-    inline int64_t const BUF_SIZE = 262144;
+    inline int64_t constexpr BUF_SIZE = 2 * 1024 * 1024;
 
     inline static bool IsPositionEmpty(const uint8_t *memory, uint32_t const entry_len)
     {
@@ -45,23 +50,45 @@ namespace UniformSort {
     {
         uint64_t const memory_len = Util::RoundSize(num_entries) * entry_len;
         auto const swap_space = std::make_unique<uint8_t[]>(entry_len);
-        auto const buffer = std::make_unique<uint8_t[]>(BUF_SIZE);
+        auto buffer = std::make_unique<uint8_t[]>(BUF_SIZE);
+        auto next_buffer = std::make_unique<uint8_t[]>(BUF_SIZE);
         uint64_t bucket_length = 0;
+
+        uint64_t buf_size = 0;
+        uint64_t next_buf_size = 0;
+        uint64_t buf_ptr = 0;
+        uint64_t swaps = 0;
+        uint64_t read_pos = input_disk_begin;
+        const uint64_t full_buffer_size = (uint64_t)BUF_SIZE / entry_len;
+
+        std::future<bool> next_read_job;
+
+        auto read_next = [&input_disk, full_buffer_size, num_entries, entry_len, &read_pos, input_disk_begin, &next_buffer, &next_buf_size, &next_read_job]() {
+            next_buf_size = std::min(full_buffer_size, num_entries - ((read_pos - input_disk_begin) / entry_len));
+            const uint64_t read_size = next_buf_size * entry_len;
+            if (read_size == 0) {
+              return;
+            }
+            next_read_job = pool.submit([&input_disk, read_pos, read_size, buffer = next_buffer.get()]{input_disk.Read(read_pos, buffer, read_size);});
+            read_pos += next_buf_size * entry_len;
+        };
+
+        // Start reading the first block. Memset will take a while.
+        read_next();
+
         // The number of buckets needed (the smallest power of 2 greater than 2 * num_entries).
         while ((1ULL << bucket_length) < 2 * num_entries) bucket_length++;
         memset(memory, 0, memory_len);
 
-        uint64_t read_pos = input_disk_begin;
-        uint64_t buf_size = 0;
-        uint64_t buf_ptr = 0;
-        uint64_t swaps = 0;
         for (uint64_t i = 0; i < num_entries; i++) {
             if (buf_size == 0) {
-                // If read buffer is empty, read from disk and refill it.
-                buf_size = std::min((uint64_t)BUF_SIZE / entry_len, num_entries - i);
+                // If read buffer is empty, wait for the reader thread, get the
+                // buffer and immediately start reading the next block.
+                next_read_job.wait();
+                buf_size = next_buf_size;
+                buffer.swap(next_buffer);
                 buf_ptr = 0;
-                input_disk.Read(read_pos, buffer.get(), buf_size * entry_len);
-                read_pos += buf_size * entry_len;
+                read_next();
             }
             buf_size--;
             // First unique bits in the entry give the expected position of it in the sorted array.
