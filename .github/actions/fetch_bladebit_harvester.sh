e1ed1dfc8ab0a97b6abc308ab0bf351772100859
Harold Brenes <h.brenes@chia.net>
2023-06-28 18:00:24 -0400

    Add action to fetch bladebit harvester

---
+++ .github/actions/fetch_bladebit_harvester.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+set -eo pipefail
+_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+cd "$_dir/../.."
+
+##
+# Usage: fetch_bladebit_harvester.sh <linux|macos|windows> <arm64|x86-64>
+#
+# Use gitbash or similar under Windows.
+##
+host_os=$1
+host_arch=$2
+
+if [[ "${host_os}" != "linux" ]] && [[ "${host_os}" != "macos" ]] && [[ "${host_os}" != "windows" ]]; then
+  >&2 echo "Unkonwn OS '${host_os}'"
+  exit 1
+fi
+
+if [[ "${host_arch}" != "arm64" ]] && [[ "${host_arch}" != "x86-64" ]]; then
+  >&2 echo "Unkonwn Architecture '${host_arch}'"
+  exit 1
+fi
+
+# Update these when pointing to different releases
+artifact_ver="v3.0.0-alpha4"
+artifact_base_url="https://github.com/harold-b/bladebit-test/releases/download/v3-alpha4-fixes"
+
+linux_sha256=
+macos_sha256=
+windows_sha256=
+
+artifact_ext="tar.gz"
+sha_bin="sha256sum"
+expected_sha256=
+
+case "${host_os}" in
+  linux)
+    expected_sha256=$linux_sha256
+  ;;
+  macos)
+    expected_sha256=$macos_sha256
+    sha_bin="shasum -a 256"
+  ;;
+  windows)
+    expected_sha256=$windows_sha256
+    artifact_ext="zip"
+  ;;
+  *)
+    >&2 echo "Unexpected OS '${host_os}'"
+    exit 1
+  ;;
+esac
+
+# Download artifact
+artifact_name="green_reaper.${artifact_ext}"
+curl -L "${artifact_base_url}/green_reaper-${artifact_ver}-${host_os}-${host_arch}.${artifact_ext}" > "${artifact_name}"
+
+# Validate sha256, if one was given
+if [ ! -z "${expected_sha256}" ]; then
+  gr_sha256="$(${sha_bin} ${artifact_name})"
+
+  if [[ "${gr_sha256}" != "${expected_sha256}" ]]; then
+    >&2 echo "GreenReaper SHA256 mismatch!"
+    >&2 echo " Got     : '${gr_sha256}'"
+    >&2 echo " Expected: '${expected_sha256}'"
+    exit 1
+  fi
+fi
+
+# Unpack artifact
+dst_dir="libs/green_reaper"
+mkdir -p "${dst_dir}"
+if [[ "${artifact_ext}" == "zip" ]]; then
+
+  unzip -d "${dst_dir}" "${artifact_name}"
+else
+  pushd "${dst_dir}"
+  tar -xzvf "../${artifact_name}"
+  popd
+fi
+
