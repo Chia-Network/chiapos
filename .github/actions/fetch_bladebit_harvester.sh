f98488bbcc1dfc784646425c9d49485037ca2bcf
William Allen <wallentx@users.noreply.github.com>
2023-06-29 16:42:09 -0500

    Enabling all builds

--- .github/actions/fetch_bladebit_harvester.sh
+++ .github/actions/fetch_bladebit_harvester.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eo pipefail
-_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 cd "$_dir/../.."
 
 ##
@@ -12,12 +12,12 @@ host_os=$1
 host_arch=$2
 
 if [[ "${host_os}" != "linux" ]] && [[ "${host_os}" != "macos" ]] && [[ "${host_os}" != "windows" ]]; then
-  >&2 echo "Unkonwn OS '${host_os}'"
+  echo >&2 "Unkonwn OS '${host_os}'"
   exit 1
 fi
 
 if [[ "${host_arch}" != "arm64" ]] && [[ "${host_arch}" != "x86-64" ]]; then
-  >&2 echo "Unkonwn Architecture '${host_arch}'"
+  echo >&2 "Unkonwn Architecture '${host_arch}'"
   exit 1
 fi
 
@@ -34,35 +34,35 @@ sha_bin="sha256sum"
 expected_sha256=
 
 case "${host_os}" in
-  linux)
-    expected_sha256=$linux_sha256
+linux)
+  expected_sha256=$linux_sha256
   ;;
-  macos)
-    expected_sha256=$macos_sha256
-    sha_bin="shasum -a 256"
+macos)
+  expected_sha256=$macos_sha256
+  sha_bin="shasum -a 256"
   ;;
-  windows)
-    expected_sha256=$windows_sha256
-    artifact_ext="zip"
+windows)
+  expected_sha256=$windows_sha256
+  artifact_ext="zip"
   ;;
-  *)
-    >&2 echo "Unexpected OS '${host_os}'"
-    exit 1
+*)
+  echo >&2 "Unexpected OS '${host_os}'"
+  exit 1
   ;;
 esac
 
 # Download artifact
 artifact_name="green_reaper.${artifact_ext}"
-curl -L "${artifact_base_url}/green_reaper-${artifact_ver}-${host_os}-${host_arch}.${artifact_ext}" > "${artifact_name}"
+curl -L "${artifact_base_url}/green_reaper-${artifact_ver}-${host_os}-${host_arch}.${artifact_ext}" >"${artifact_name}"
 
 # Validate sha256, if one was given
-if [ ! -z "${expected_sha256}" ]; then
+if [ -n "${expected_sha256}" ]; then
   gr_sha256="$(${sha_bin} ${artifact_name})"
 
   if [[ "${gr_sha256}" != "${expected_sha256}" ]]; then
-    >&2 echo "GreenReaper SHA256 mismatch!"
-    >&2 echo " Got     : '${gr_sha256}'"
-    >&2 echo " Expected: '${expected_sha256}'"
+    echo >&2 "GreenReaper SHA256 mismatch!"
+    echo >&2 " Got     : '${gr_sha256}'"
+    echo >&2 " Expected: '${expected_sha256}'"
     exit 1
   fi
 fi
@@ -71,11 +71,9 @@ fi
 dst_dir="libs/green_reaper"
 mkdir -p "${dst_dir}"
 if [[ "${artifact_ext}" == "zip" ]]; then
-
   unzip -d "${dst_dir}" "${artifact_name}"
 else
   pushd "${dst_dir}"
   tar -xzvf "../../${artifact_name}"
   popd
 fi
-
