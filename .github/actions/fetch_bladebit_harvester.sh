233ff3aa28fc057146412af137c7ac4c453780e9
Harold Brenes <h.brenes@chia.net>
2023-07-17 16:10:40 -0400

    Update GR hashes and fix issues in sh file

--- .github/actions/fetch_bladebit_harvester.sh
+++ .github/actions/fetch_bladebit_harvester.sh
@@ -24,23 +24,36 @@ fi
 ## Change this before releasing 2.0.0
 artifact_ver="v3.0.0-alpha4"
 artifact_base_url="https://github.com/harold-b/bladebit-test/releases/download/v3-alpha4-fixes"
-## End changes
 
-linux_sha256=
-macos_sha256=
-windows_sha256=
+linux_arm_sha256="d6f4fc6014a0faa4a5735980a4c12753e205e6429a9d92ea1d32831e475df357"
+linux_x86_sha256="98c0efb464935455595d7a573027613c46a7921a95d3f9216b27814f84a1ac31"
+macos_arm_sha256="713849aa5d01df9ccfcec259920d86a4ddec83cc3bbc30860852dcf9ac793978"
+macos_x86_sha256="67885e4f95b11a115f37138309cc9fd92785229f76adefe6d23e15c45625c92f"
+windows_sha256="2a951daa19c65574808537007b874a29d9001bc4387c162633646b0f546676ca"
+## End changes
 
 artifact_ext="tar.gz"
 sha_bin="sha256sum"
 expected_sha256=
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sha_bin="shasum -a 256"
+fi
+
 case "${host_os}" in
 linux)
-  expected_sha256=$linux_sha256
+  if [[ "${host_arch}" == "arm64" ]]; then  
+    expected_sha256=$linux_arm_sha256
+  else
+    expected_sha256=$linux_x86_sha256
+  fi
   ;;
 macos)
-  expected_sha256=$macos_sha256
-  sha_bin="shasum -a 256"
+  if [[ "${host_arch}" == "arm64" ]]; then  
+    expected_sha256=$macos_arm_sha256
+  else
+    expected_sha256=$macos_x86_sha256
+  fi
   ;;
 windows)
   expected_sha256=$windows_sha256
@@ -58,7 +71,7 @@ curl -L "${artifact_base_url}/green_reaper-${artifact_ver}-${host_os}-${host_arc
 
 # Validate sha256, if one was given
 if [ -n "${expected_sha256}" ]; then
-  gr_sha256="$(${sha_bin} ${artifact_name})"
+  gr_sha256="$(${sha_bin} ${artifact_name} | cut -d' ' -f1)"
 
   if [[ "${gr_sha256}" != "${expected_sha256}" ]]; then
     echo >&2 "GreenReaper SHA256 mismatch!"
