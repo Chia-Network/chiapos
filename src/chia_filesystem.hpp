8c2543a9b1d24c863e26754304c8927fa87c42bf
Earle Lowe <e.lowe@chia.net>
2024-09-11 09:48:24 -0700

    Just use std::filesystem

--- src/chia_filesystem.hpp
+++ src/chia_filesystem.hpp
@@ -15,23 +15,7 @@
 #ifndef SRC_CPP_CHIA_FILESYSTEM_HPP_
 #define SRC_CPP_CHIA_FILESYSTEM_HPP_
 
-#ifdef __APPLE__
-// std::filesystem is not supported on Mojave
-#include "filesystem.hpp"
-namespace fs = ghc::filesystem;
-#else
-#ifdef __has_include
-#if __has_include(<filesystem>)
 #include <filesystem>
 namespace fs = std::filesystem;
-#else
-#include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
-#endif
-#else
-#include <filesystem>
-namespace fs = std::filesystem;
-#endif
-#endif
 
 #endif // SRC_CPP_CHIA_FILESYSTEM_HPP_
